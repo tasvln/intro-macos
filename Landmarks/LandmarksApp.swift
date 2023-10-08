30059882abf40a3938ee8e2c38afa4c37a4ad0ec
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-10-08 15:32:27 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Temitope Adebayo on 2023-10-08.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
